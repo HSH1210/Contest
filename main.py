30eba6ea07cc222a67fa867b4a3265dff147a3c5
JMdJJ0806 <61314458+JMdJJ0806@users.noreply.github.com>
2023-07-08 16:26:36 +0900

    Update main.py
    chMove

--- main.py
+++ main.py
@@ -1,33 +1,57 @@
-import pygame # pygame 모듈의 임포트
-import sys # 외장 모듈
-from pygame.locals import * # QUIT 등의 pygame 상수들을 로드한다.
+import pygame
+import sys
 import random
 
-width = 1600 # 상수 설정
+width = 1600
 height = 900    
 white = (255, 255, 255)
 black = (0, 0, 0)
 gray = (204, 204, 204)
 
-character_size = 50 # 캐릭터
-charspeed = 0.6
-character = pygame.image.load('character.jpg')
-character = pygame.transform.scale(character, (character_size, character_size))
-r1 = pygame.Rect(width / 2 - 400, height / 2 - 300, character_size, character_size)
+ch_size = 50
+ch_speed = 0.1
+maxspeed = 2.5
+ch = pygame.image.load('character.jpg')
+ch = pygame.transform.scale(ch, (ch_size, ch_size))
+chx, chy, chsx, chsy = 0, 0, 0, 0
 
-def characterMove():
+
+def chMove():
+    global chx, chy, chsx, chsy
     keyInput = pygame.key.get_pressed()
-    if keyInput[pygame.K_a] and r1.left >= 0:
-        r1.x -= charspeed
-    elif keyInput[pygame.K_d] and r1.right <= width:
-        r1.x += charspeed
-    elif keyInput[pygame.K_w] and r1.top >= 0:
-        r1.y -= charspeed
-    elif keyInput[pygame.K_s] and r1.bottom <= height:
-        r1.y += charspeed
+    if keyInput[pygame.K_a]:
+        chsx -= ch_speed
+    if keyInput[pygame.K_d]:
+        chsx += ch_speed
+    if keyInput[pygame.K_w]:
+        chsy -= ch_speed
+    if keyInput[pygame.K_s]:
+        chsy += ch_speed
+
+    chsx = max(-maxspeed, min(maxspeed, chsx))
+    chsy = max(-maxspeed, min(maxspeed, chsy))
+
+    if chsx < 0:
+        chsx += ch_speed/2
+    elif chsx > 0:
+        chsx -= ch_speed/2
+    if chsy < 0:
+        chsy += ch_speed/2
+    elif chsy > 0:
+        chsy -= ch_speed/2
+
+    chx = round(chx, 2)
+    chy = round(chy, 2)
+    chsx = round(chsx, 2)
+    chsy = round(chsy, 2)
+
+    chx += chsx
+    chy += chsy
+    print(chx,chy,chsx,chsy)
+
 
 def initGame():
-    global screen, clock, character
+    global screen, clock, ch
     pygame.init()
     screen = pygame.display.set_mode((width, height))
     pygame.display.set_caption('Game') # 창 제목 설정
@@ -35,7 +59,7 @@ def initGame():
 
 
 def runGame():
-    global screen, clock, character, fps
+    global screen, clock, ch, fps
     fps = clock.tick(60)
     onGame = True
     while onGame:
@@ -44,9 +68,9 @@ def runGame():
                 pygame.quit() # pygame을 종료한다
                 sys.exit() # 창을 닫는다
 
-        characterMove() #캐릭터 움직임
+        chMove() # 캐릭터 움직임
         screen.fill(gray) # screen를 하얀색으로 채운다
-        screen.blit(character, r1)    
+        screen.blit(ch, pygame.Rect(int(chx), int(chy), ch_size, ch_size)) # 캐릭터 blit   
         pygame.display.update() # 화면을 업데이트한다
 
 initGame()
